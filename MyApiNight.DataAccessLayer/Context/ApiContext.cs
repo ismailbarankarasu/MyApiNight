29aa4c5719ae30a38cb92d21a5770450f061d9b4
İsmail Baran <ismailbaran04@gmail.com>
2024-12-06 21:23:35 +0300

    context tamamlandı

---
+++ MyApiNight.DataAccessLayer/Context/ApiContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyApiNight.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApiNight.DataAccessLayer.Context
+{
+    public class ApiContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-AV376HC\\SQLEXPRESS;initial Catalog=ApiDbNight;integrated Security=true");
+        }
+        public DbSet<Category> Categories{ get; set; }
+        public DbSet<Product> Products{ get; set; }
+    }
+}
