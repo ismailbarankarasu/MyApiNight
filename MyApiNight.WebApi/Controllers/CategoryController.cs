caa671a584b387b5aa4e0c63c8b73606464bfe82
İsmail Baran <ismailbaran04@gmail.com>
2024-12-12 20:56:37 +0300

    category ve product controller tammalandı

--- MyApiNight.WebApi/Controllers/CategoryController.cs
+++ MyApiNight.WebApi/Controllers/CategoryController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MyApiNight.BusinessLayer.Abstract;
+using MyApiNight.EntityLayer.Concrete;
 
 namespace MyApiNight.WebApi.Controllers
 {
@@ -7,5 +9,41 @@ namespace MyApiNight.WebApi.Controllers
     [ApiController]
     public class CategoryController : ControllerBase
     {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        [HttpGet]
+        public IActionResult CategoryList()
+        {
+            var values = _categoryService.TGetAll();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult CreateCategory(Category category)
+        {
+            _categoryService.TInsert(category);
+            return Ok("Ekleme Başarılı");
+        }
+        [HttpDelete]
+        public IActionResult DeleteCategory(int id) 
+        {
+            _categoryService.TDelete(id);
+            return Ok("Silme Başarılı");
+        }
+        [HttpPut]
+        public IActionResult UpdateCategory(Category category)
+        {
+            _categoryService.TUpdate(category);
+            return Ok("Güncelleme Yapıldı");
+        }
+        [HttpGet("GetCategory")]
+        public IActionResult GetCategory(int id)
+        {
+            var value = _categoryService.TGetById(id);
+            return Ok(value);
+        }
     }
 }
