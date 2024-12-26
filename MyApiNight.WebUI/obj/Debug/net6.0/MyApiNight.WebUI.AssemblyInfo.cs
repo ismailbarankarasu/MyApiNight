d10fd41d8aed0eb92f277168a01a358455381b8c
İsmail Baran <ismailbaran04@gmail.com>
2024-12-26 23:21:33 +0300

    otomatik güncellemeler işlendi

--- MyApiNight.WebUI/obj/Debug/net6.0/MyApiNight.WebUI.AssemblyInfo.cs
+++ MyApiNight.WebUI/obj/Debug/net6.0/MyApiNight.WebUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApiNight.WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb6b6fe799fd64647e219f34b24fd4f6c5ff12a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a66a182709037ce8af3d477cb1241bb1beebca6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApiNight.WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApiNight.WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
